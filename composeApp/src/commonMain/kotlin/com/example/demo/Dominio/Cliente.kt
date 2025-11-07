0b14201c883c6b63f89ddf209d4404a9a6ca5ccb
cristz-tsh350 <cristobalpes2017@gmail.com>
2025-11-06 23:14:20 -0300

    primer commit

--- composeApp/src/commonMain/kotlin/com/example/demo/Dominio/Cliente.kt
+++ composeApp/src/commonMain/kotlin/com/example/demo/Dominio/Cliente.kt
@@ -1,22 +1,4 @@
-
-
 package com.example.demo.Dominio
 
-// Cliente hereda de Persona [cite: 102]
-class Cliente(
-    rut: String,
-    nombre: String,
-    email: String,
-    val direccionFacturacion: String,
-    var estado: EstadoCliente
-) : Persona(rut, nombre, email) {
-
-    // Simulación de los campos
-    override val id: String = rut
-    // Simplemente ponemos un string vacío
-    override val createdAt: String = "" // <-- Cambiado
-    override val updatedAt: String = ""
-
-    // Cliente también implementa ExportablePDF [cite: 156]
-    // (Lógica de toPdfTable no implementada)
+class Cliente {
 }
---
+++ composeApp/src/commonMain/kotlin/com/example/demo/Persistencia/PdfService.kt
@@ -0,0 +1,4 @@
+package com.example.demo.Persistencia
+
+class PdfService {
+}
